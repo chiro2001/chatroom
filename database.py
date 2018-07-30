aaf5bad9698e5d9072be459acec5cd148753ec0a
root <root@LanceLiang2018.xyz>
2018-07-30 21:38:01 +0800

    Second

---
+++ database.py
@@ -0,0 +1,124 @@
+import sqlite3 as sql
+
+conn_user = sql.connect('database.db', check_same_thread=False)
+conn_entry = sql.connect('entries.db', check_same_thread=False)
+
+
+def user_add(name, passwd, email):
+    c = conn_user.cursor()
+    c.execute('select * from data where name = ?;', (name,))
+    data = c.fetchall()
+    if len(data) != 0:
+        return 'Name used'
+    try:
+        c.execute('insert into data values (?, ?, ?);', (name, passwd, email))
+    except Exception as e:
+        conn_user.commit()
+        c.close()
+        return 'Fail, ' + str(e)
+    conn_user.commit()
+    c.close()
+    return 'Success'
+
+
+def user_del(name, passwd):
+    c = conn_user.cursor()
+    c.execute('select * from data where name = ?;', (name, ))
+    data = c.fetchall()
+    if len(data) == 0:
+        conn_user.commit()
+        c.close()
+        return 'No such of user'
+    if data[0][1] != passwd:
+        conn_user.commit()
+        c.close()
+        return 'Password error'
+    c.execute('delete from data where name = ?;', (name, ))
+    conn_user.commit()
+    c.close()
+    return 'Success'
+
+
+def user_check(name, passwd):
+    c = conn_user.cursor()
+    c.execute('select * from data where name = ?;', (name, ))
+    data = c.fetchall()
+    c.close()
+    if len(data) == 0:
+        return 'No such of user'
+    if data[0][1] != passwd:
+        return 'Password error'
+    return 'Success'
+
+
+def user_get_email(name):
+    c = conn_user.cursor()
+    conn_user.commit()
+    c.execute('select email from data where name = ?;', (name, ))
+    data = c.fetchall()
+    if len(data) == 0:
+        return 'Get None'
+    c.close()
+    return data[0][0]
+
+
+def user_all_name():
+    c = conn_user.cursor()
+    c.execute('select name from data')
+    data = c.fetchall()
+    c.close()
+    if len(data) == 0:
+        return []
+    return data
+
+
+def user_all():
+    c = conn_user.cursor()
+    c.execute('select name, email from data')
+    data = c.fetchall()
+    c.close()
+    if len(data) == 0:
+        return []
+    return data
+
+
+# 设置每页20条
+one_page = 20
+
+
+def entry_get(page):
+    c = conn_entry.cursor()
+    c.execute('select * from entries order by id desc limit ? offset ?;', (one_page, one_page * page))
+    data = c.fetchall()
+    conn_entry.commit()
+    c.close()
+    return data
+
+
+def entry_insert(sid, name, time, icon, message):
+    c = conn_entry.cursor()
+    try:
+        c.execute('insert into entries values (?, ?, ?, ?, ?)', (sid, name, time, icon, message))
+    except Exception as e:
+        return 'Fail, ' + str(e)
+    finally:
+        conn_entry.commit()
+        c.close()
+    return 'Success'
+
+
+def entry_get_new_id():
+    c = conn_entry.cursor()
+    c.execute('select nid from sid')
+    data = c.fetchall()
+    nid = data[0][0]
+    c.execute('update sid set nid = ? where s = 0;', (nid + 1, ))
+    conn_entry.commit()
+    c.close()
+    return nid
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        print(entry_get_new_id())
+
---
+++ sever.py
@@ -0,0 +1,121 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+import hashlib
+from database import *
+import time
+
+app = Flask(__name__)
+
+entries = []
+users = []
+
+
+def get_icon(email):
+    return'https://s.gravatar.com/avatar/' + hashlib.md5(email.encode()).hexdigest() + '?s=144'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    sever_init()
+    if request.method == 'GET':
+        if not 'username' in session:
+            return redirect(url_for('login'))
+        return render_template('ChatRoom.html', username=session['username'],
+                               icon=session['icon'],
+                               entries=entries,
+                               users=users)
+    if request.method == 'POST':
+        timedata = time.localtime(time.time())
+        data = {
+            'username': session['username'],
+            'message': request.form['message'],
+            'time': str(timedata.tm_mon).zfill(2) + '/' + str(timedata.tm_mday).zfill(2) + ' ' + \
+                    str(timedata.tm_hour).zfill(2) + ':' + str(timedata.tm_min).zfill(2)
+        }
+        entry_insert(entry_get_new_id(), data['username'], data['time'], get_icon(session['email']), data['message'])
+        return redirect(url_for('index'))
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if 'username' in session:
+        redirect(url_for('logout'))
+    if request.method == 'POST':
+        if request.form['passwd'] != request.form['passwd_']:
+            return '两次密码不一致。' + '<a href=%s>返回</a>' % url_for('signup')
+        if request.form['username'] == '':
+            return '用户名不能为空。' + '<a href=%s>返回</a>' % url_for('signup')
+        hl = hashlib.md5(request.form['passwd'].encode()).hexdigest()
+        result = user_add(request.form['username'], hl, request.form['email'].lower())
+        sever_user_init()
+        return result + '<a href=%s>首页</a>' % url_for('index')
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'username' in session:
+        redirect(url_for('logout'))
+    if request.method == 'POST':
+        hl = hashlib.md5(request.form['passwd'].encode()).hexdigest()
+        result = user_check(request.form['username'], hl)
+        if result != 'Success':
+            return result
+        session['username'] = request.form['username']
+        session['passwd'] = hl
+        session['email'] = user_get_email(session['username'])
+        session['icon'] = get_icon(session['email'])
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/get_email/<username>')
+def get_email(username):
+    email = user_get_email(username)
+    return email + '<br><img src=\"%s\">' % (get_icon(email))
+
+
+@app.route('/history/<int:page>', methods=['GET', 'POST'])
+def get_history(page):
+    if not 'username' in session:
+        return redirect(url_for('login'))
+    if request.method == 'GET':
+        get_entries = entry_get(page)
+        return render_template('History.html', entries=get_entries)
+    if request.method == 'POST':
+        get_page = request.form['page']
+        return redirect('/history/' + get_page)
+
+
+@app.route('/about')
+def about():
+    return redirect('http://lanceliang2018.xyz/index.php/2018/07/30/chat-room/')
+
+
+def sever_user_init():
+    global users
+    users = user_all_name()
+
+
+def sever_entry_init():
+    global entries
+    entries = list(entry_get(0))
+
+
+def sever_init():
+    sever_user_init()
+    sever_entry_init()
+
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+if __name__ == '__main__':
+    sever_init()
+    app.run(debug=False, host='0.0.0.0', port=10086)
