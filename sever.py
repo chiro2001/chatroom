4709d76c00e28046847969493cf0346b53a31de8
Lance Liang <LanceLiang2018@163.com>
2019-06-07 15:35:26 +0800

    Special 邮件关照

--- sever.py
+++ sever.py
@@ -7,6 +7,9 @@ import threading
 import os
 import pytz
 from datetime import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
 
 app = Flask(__name__)
@@ -64,6 +67,30 @@ def index(room: str):
         }
         entry_insert(entry_get_new_id(), data['username'], data['time'],
                      get_icon(session['email']), data['message'], room)
+        if session['email'].lower() == 'lanceliang2018@163.com' and room == 'special':
+            my_sender = 'LanceLiang2018@163.com'  # 发件人邮箱账号
+            my_pass = '1352040930smtp'  # 发件人邮箱密码
+            # my_user = '1352040930@qq.com'  # 收件人邮箱账号
+            users_ = user_all()
+            users_to_send = []
+            for user in users_:
+                if '_' in user[0]:
+                    users_to_send.append(user)
+            for user in users_to_send:
+                try:
+                    # print('try to send:', user)
+                    msg = MIMEText(data['message'], 'plain', 'utf-8')
+                    msg['From'] = formataddr(["Lance Liang", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+                    msg['To'] = formataddr([user[0], user[1]])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+                    msg['Subject'] = "来自 Lance 的新消息"  # 邮件的主题，也可以说是标题
+
+                    server = smtplib.SMTP_SSL("smtp.163.com", 465)  # 发件人邮箱中的SMTP服务器，端口是465
+                    server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+                    server.sendmail(my_sender, [user[1], ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+                    server.quit()  # 关闭连接
+                except Exception as e:
+                    print(e)
+
         return redirect('/%s' % room)
 
 
